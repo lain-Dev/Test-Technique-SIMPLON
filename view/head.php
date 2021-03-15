155200fcefb31b0370a837eb5287f58b29a1de0d
olivier lebon <olivier@lebon.dev>
2021-03-15 09:42:51 +0400

    début de la création de apply
    création des dossiers et fichiers présumés pour apply

---
+++ view/head.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;1,200&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Vidaloka&display=swap" rel="stylesheet">
+    <!-- link pour la map -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+    integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+    crossorigin="" />
+    <link rel="stylesheet" href="../style/style.css">
+    <!--Font awesome-->
+    <script src="https://use.fontawesome.com/c18e5332f2.js"></script>
+    <!--AOS Animate on scroll library-->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <title>ZotColoc</title>
+
+</head>
+
+<body>
+    
+    <header>
+        <nav>
+        
+        
+        </nav>
+    </header>
---
+++ view/home.php
@@ -0,0 +1,11 @@
+<?php require_once(dirname(__DIR__).'/view/head.php');?>
+
+
+
+
+
+
+
+
+<?php require_once(dirname(__DIR__).'/view/footer.php');?>
+
