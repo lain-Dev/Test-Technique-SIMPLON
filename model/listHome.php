6278edc54ea734547913ffea239271a991ebc59c
olivier lebon <olivier@lebon.dev>
2021-03-19 11:24:48 +0400

    affichage poste sur home page

---
+++ model/listHome.php
@@ -0,0 +1,26 @@
+<?php foreach($postes as $poste):?>
+    <div class="container text-center mb-1" style="margin-top: 40px;">
+      <div class="jumbotron">
+
+        <table class="table table-striped">
+            <h3>Poste: <?= htmlspecialchars($poste->poste); ?></h3>
+            <thead>
+                <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Plage horaire</th>
+                <th scope="col">Numéro membre</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>15/03/2021</td>
+                <td>8h à 9h</td>
+                <td>465</td>
+            </tbody>
+            </table>
+            <!---->
+        </div>
+        <div class="push-footer"></div>
+    </div>
+
+<?php endforeach; ?>  
--- view/home.php
+++ view/home.php
@@ -55,29 +55,12 @@
 
 
 
-<div class="container text-center" style="margin-top: 40px;">
-      <div class="jumbotron">
-        <!--exemple poste-->
-        <table class="table table-striped">
-          <h3>POSTE 01</h3>
-          <thead>
-            <tr>
-              <th scope="col">Date</th>
-              <th scope="col">Plage horaire</th>
-              <th scope="col">Numéro membre</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>15/03/2021</td>
-              <td>8h à 9h</td>
-              <td>465</td>
-          </tbody>
-        </table>
-        <!---->
-      </div>
-    <div class="push-footer"></div>
-</div>
+
+        
+      <?php require_once(dirname(__DIR__).'/controller/showPostes.php'); ?>	
+			<?php require_once(dirname(__DIR__).'/model/listHome.php'); ?>
+
+
 
 
 
